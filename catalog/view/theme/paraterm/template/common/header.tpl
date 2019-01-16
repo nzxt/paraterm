7923adfb85a119f29981857b656f459f19df3af6
Deniz True <nzxt.ua@gmail.com>
2019-01-17 00:30:11 +0200

    gulp updated to v4 // fluid templates // npm bootstrap added

--- catalog/view/theme/paraterm/template/common/header.tpl
+++ catalog/view/theme/paraterm/template/common/header.tpl
@@ -17,16 +17,21 @@
 <?php if ($keywords) { ?>
 <meta name="keywords" content= "<?php echo $keywords; ?>" />
 <?php } ?>
+<!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script> -->
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script> -->
 <script src="catalog/view/javascript/jquery/jquery-2.1.1.min.js" type="text/javascript"></script>
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
 <link href="catalog/view/javascript/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
 <script src="catalog/view/javascript/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 <link href="catalog/view/javascript/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-<link href="//fonts.googleapis.com/css?family=Open+Sans:400,400i,300,700" rel="stylesheet" type="text/css" />
-<link href="catalog/view/theme/default/stylesheet/stylesheet.css" rel="stylesheet">
-<link href="catalog/view/theme/paraterm/stylesheet/stylesheet.css" rel="stylesheet">
+<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"> -->
+<!-- <link href="//fonts.googleapis.com/css?family=Open+Sans:400,400i,300,700" rel="stylesheet" type="text/css" /> -->
+<!-- <link href="catalog/view/theme/default/stylesheet/stylesheet.css" rel="stylesheet"> -->
 <?php foreach ($styles as $style) { ?>
 <link href="<?php echo $style['href']; ?>" type="text/css" rel="<?php echo $style['rel']; ?>" media="<?php echo $style['media']; ?>" />
 <?php } ?>
+<link href="catalog/view/theme/paraterm/stylesheet/stylesheet.css" rel="stylesheet">
 <script src="catalog/view/javascript/common.js" type="text/javascript"></script>
 <?php foreach ($links as $link) { ?>
 <link href="<?php echo $link['href']; ?>" rel="<?php echo $link['rel']; ?>" />
@@ -80,8 +85,7 @@
           <?php } ?>
         </div>
       </div>
-      <div class="col-sm-5"><?php echo $search; ?>
-      </div>
+      <div class="col-sm-5"><?php echo $search; ?></div>
       <div class="col-sm-3"><?php echo $cart; ?></div>
     </div>
   </div>
--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,56 @@
-// Подключаем Gulp и все необходимые библиотеки
-var gulp           = require('gulp'),
-		gutil          = require('gulp-util' ),
-		sass           = require('gulp-sass'),
-		browserSync    = require('browser-sync'),
-		cleanCSS       = require('gulp-clean-css'),
-		autoprefixer   = require('gulp-autoprefixer'),
-		bourbon        = require('node-bourbon'),
-		ftp            = require('vinyl-ftp');
+var gulp         = require('gulp'),
+    browserSync  = require('browser-sync').create(),
+    sass         = require('gulp-sass'),
+    cleanCSS     = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    bourbon      = require('node-bourbon'),
+    ftp          = require('vinyl-ftp');
+
+var reload = browserSync.reload;
 
 // Обновление страниц сайта на локальном сервере
 gulp.task('browser-sync', function() {
-	browserSync({
-		proxy: "termopara.net",
-		notify: false
-	});
+  browserSync.init({
+    proxy: "termopara.net",
+    notify: false
+  });
 });
 
-// Компиляция stylesheet.css
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-	return gulp.src('catalog/view/theme/paraterm/stylesheet/stylesheet.sass')
-		.pipe(sass({
-			includePaths: bourbon.includePaths
-		}).on('error', sass.logError))
-		.pipe(autoprefixer(['last 15 versions']))
-		.pipe(cleanCSS())
-		.pipe(gulp.dest('catalog/view/theme/paraterm/stylesheet/'))
-		.pipe(browserSync.reload({stream: true}))
+  return gulp.src("catalog/view/theme/paraterm/sass/stylesheet.sass")
+    .pipe(sass.sync({
+      includePaths: bourbon.includePaths,
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer(['last 15 versions']))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("catalog/view/theme/paraterm/stylesheet/"))
+    .pipe(browserSync.stream());
 });
 
 // Наблюдение за файлами
-gulp.task('watch', gulp.series('sass', 'browser-sync', function() {
-	gulp.watch('catalog/view/theme/paraterm/stylesheet/stylesheet.sass', ['sass']);
-	gulp.watch('catalog/view/theme/paraterm/template/**/*.tpl', browserSync.reload);
-	gulp.watch('catalog/view/theme/paraterm/javascript/**/*.js', browserSync.reload);
-	gulp.watch('catalog/view/theme/paraterm/libs/**/*', browserSync.reload);
-}));
-
-// Выгрузка изменений на хостинг
-gulp.task('deploy', function() {
-	var conn = ftp.create({
-		host:      'hostname.com',
-		user:      'username',
-		password:  'userpassword',
-		parallel:  10,
-		log: gutil.log
-	});
-	var globs = [
-		'catalog/view/theme/paraterm/**'
-	];
-	return gulp.src(globs, { buffer: false })
-		.pipe(conn.dest('/path/to/folder/on/server'));
+gulp.task('watch', function() {
+  gulp.watch('catalog/view/theme/paraterm/sass/**/*.sass', gulp.parallel('sass'));
+  gulp.watch('catalog/view/theme/paraterm/template/**/*.tpl', reload);
+  gulp.watch(['catalog/view/theme/paraterm/javascript/**/*.js', 'catalog/view/javascript/common.js'], reload);
+  gulp.watch('catalog/view/theme/paraterm/libs/**/*.js', reload);
 });
 
-gulp.task('default', gulp.series('watch'));
+// Выгрузка изменений на хостинг
+// gulp.task('deploy', function() {
+//   var conn = ftp.create({
+//     host:      'hostname.com',
+//     user:      'username',
+//     password:  'userpassword',
+//     parallel:  10,
+//     log: gutil.log
+//   });
+//   var globs = [
+//     'catalog/view/theme/paraterm/**'
+//   ];
+//   return gulp.src(globs, { buffer: false })
+//         .pipe(conn.dest('/path/to/folder/on/server'));
+// });
+
+gulp.task('default', gulp.parallel('watch', 'sass', 'browser-sync'));
